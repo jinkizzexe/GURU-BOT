3e93d26d46d979c7ae490b41bae530c0cb4ee6c6
OMAR ! <156843341+jinkizzexe@users.noreply.github.com>
2024-01-20 22:29:36 +0100

    Update av-audios.js

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -4,7 +4,7 @@ handler.all = async function (m) {
   if (/^.menu$/i.test(m.text) ) {
     let av = './Assets/mp3/menu1.mp3'
     this.sendPresenceUpdate('recording', m.chat);
-    this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+    this.sendFile(m.chat, av, 'menu1.mp3', null, m, true, { type: 'audioMessage', ptt: true })
    }
 
   if (/^bgm$/i.test(m.text) ) {
