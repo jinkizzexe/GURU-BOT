0d5b8b768a7e61e028c03e615e7b0fcbb5dbe343
OMAR ! <156843341+jinkizzexe@users.noreply.github.com>
2024-01-20 16:51:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,17 +6,17 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 
-global.owner =
-  ['212601291320', 'OMAR CHARAF !', true],
-  ['6281255369012', 'Ash', false], 
+global.owner = [
+  ['917605902011', 'GURU', true],
+  ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['212601291320','212601291320'] 
-global.prems = ['212601291320', '212601291320', '212601291320']
-global.allowed = ['212601291320', '212601291320', '212601291320']
+global.mods = ['917605902011','32460248586'] 
+global.prems = ['917605902011', '32460248586', '919398758484']
+global.allowed = ['917605902011', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,11 +50,11 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = ' 𝑆𝐸𝑇𝐴𝑇𝐼 '
+global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
 global.premium = 'true'
-global.packname = '𝑆𝐸𝑇𝐴𝑇𝐼┃𝑂𝑀𝐴𝑅 ' 
-global.author = '@Ovmar_1' 
-global.menuvid = ''
+global.packname = 'GURU┃ᴮᴼᵀ' 
+global.author = '@Asliguru' 
+global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
@@ -65,7 +65,6 @@ global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
 
 global.wait = '*⌛ _Charging..._*\n*▰▰▰▱▱▱▱▱*'
-//global.wait = '*_لماذا لاتدعمنا على انستجرام لن تخسر أي شي https://www.instagram.com/ovmar_1_*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✅'
