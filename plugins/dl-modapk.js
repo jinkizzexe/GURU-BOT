1fd118367eff6f33ed6def94eb3921f07cb9e02a
OMAR ! <156843341+jinkizzexe@users.noreply.github.com>
2024-01-20 16:39:32 +0100

    Update dl-modapk.js

--- plugins/dl-modapk.js
+++ plugins/dl-modapk.js
@@ -2,18 +2,18 @@ import { download } from 'aptoide-scraper';
 
 let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
   try {
-    if (command === 'modapk') {
-      if (!text) throw `*[❗] Please provide the APK Name you want to download.*`;
+    if (command === 'apk') {
+      if (!text) throw `*ستاتي بوت يحتاج الى  إسم التطبيق الذي تريده*`;
 
       await conn.reply(m.chat, global.wait, m);
       let data = await download(text);
 
       if (data.size.replace(' MB', '') > 200) {
-        return await conn.sendMessage(m.chat, { text: '*[⛔] The file is too large.*' }, { quoted: m });
+        return await conn.sendMessage(m.chat, { text: '*ستاتي بوت يقول ان التطبيق كبير لإرساله ( سمح ليا )*' }, { quoted: m });
       }
 
       if (data.size.includes('GB')) {
-        return await conn.sendMessage(m.chat, { text: '*[⛔] The file is too large.*' }, { quoted: m });
+        return await conn.sendMessage(m.chat, { text: '*ستاتي يقول أن التطبيق  كبير لأرساله ( سمح ليا )*' }, { quoted: m });
       }
 
       await conn.sendMessage(
@@ -23,11 +23,11 @@ let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
       )
     }
   } catch {
-    throw `*[❗] An error occurred. Make sure to provide a valid link.*`;
+    throw `*ستاتي بوت يحتاج الى إسم التطبيق لإرساله*`;
   }
 };
 
-handler.help = ['modapk']
+handler.help = ['apk']
 handler.tags = ['downloader']
-handler.command = /^modapk$/i;
+handler.command = /^apk$/i;
 export default handler;
