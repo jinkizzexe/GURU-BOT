6efa710ca9e816373fd004c82a4418ad143fa14d
OMAR ! <156843341+jinkizzexe@users.noreply.github.com>
2024-01-20 21:56:06 +0100

    Update audio-tomp3.js

--- plugins/audio-tomp3.js
+++ plugins/audio-tomp3.js
@@ -2,7 +2,7 @@ import { toAudio } from '../lib/converter.js'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
     let q = m.quoted ? m.quoted : m
-   /* let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
+   * let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
     if (!/video|audio/.test(mime)) throw `✳️ Reply to the video or voice note you want to convert to mp3 with the command :\n\n*${usedPrefix + command}*`*/
     let media = await q.download?.()
     if (!media) throw '❎ Failed to download media'
