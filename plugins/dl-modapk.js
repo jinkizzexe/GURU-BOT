51ee9826efbfc28c91cc360cf4e1f595c0f20134
OMAR ! <156843341+jinkizzexe@users.noreply.github.com>
2024-01-21 11:54:48 +0100

    Update dl-modapk.js

--- plugins/dl-modapk.js
+++ plugins/dl-modapk.js
@@ -2,32 +2,31 @@ import { download } from 'aptoide-scraper';
 
 let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
   try {
-    if (command === 'modapk') {
-      if (!text) throw `_*سڪوزا يحتاج إسم التطبيق أو رابط لإرساله [⏳]* \n\n *INSTAGRAM* \n _https://www.instagram.com/ovmar_1_ \n *Group Whatsapp* \n _https://chat.whatsapp.com/ByIauCfwCFeE1ozctmwbS5 ._`;
+    if (command === 'تطبيق') {
+      if (!text) throw `🎯Please Provide The *Apk* Name You Want To Download.`;
 
       await conn.reply(m.chat, global.wait, m);
       let data = await download(text);
 
       if (data.size.replace(' MB', '') > 200) {
-        return await conn.sendMessage(m.chat, { text: '' }, { quoted: m });
+        return await conn.sendMessage(m.chat, { text: '*⚠️The File Is Too Large.*' }, { quoted: m });
       }
 
       if (data.size.includes('GB')) {
-        return await conn.sendMessage(m.chat, { text: '_*سڪوزا بوت التطبيق كبير جدا حاول مرة اخرى[❗]* \n\n *INSTAGRAM* \n _https://www.instagram.com/ovmar_1_ \n *Group Whatsapp* \n _https://chat.whatsapp.com/ByIauCfwCFeE1ozctmwbS5 ._' }, { quoted: m });
+        return await conn.sendMessage(m.chat, { text: '*⚠️The File Is Too Large.*' }, { quoted: m });
       }
 
       await conn.sendMessage(
         m.chat,
         { document: { url: data.dllink }, mimetype: 'application/vnd.android.package-archive', fileName: data.name + '.apk', caption: null },
         { quoted: m }
-      )
+      );
     }
   } catch {
-    throw `_*سڪوزا يحتاج إسم التطبيق أو رابط لإرساله [⏳]* \n\n *INSTAGRAM* \n _https://www.instagram.com/ovmar_1_ \n *Group Whatsapp* \n _https://chat.whatsapp.com/ByIauCfwCFeE1ozctmwbS5 ._`;
+    throw `*❌ لم أجد التطبيق الذي في الرابط حاول مجددا.*`;
   }
 };
 
-handler.help = ['modapk']
-handler.tags = ['downloader']
 handler.command = /^apk$/i;
 export default handler;
+  
