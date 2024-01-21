626f1d556b50ca7226d44c65314bf889f025297e
OMAR ! <156843341+jinkizzexe@users.noreply.github.com>
2024-01-21 12:00:25 +0100

    Update dl-modapk.js

--- plugins/dl-modapk.js
+++ plugins/dl-modapk.js
@@ -2,31 +2,32 @@ import { download } from 'aptoide-scraper';
 
 let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
   try {
-    if (command === 'تطبيق') {
-      if (!text) throw `🎯Please Provide The *Apk* Name You Want To Download.`;
+    if (command === 'modapk') {
+      if (!text) throw `*[❗] Please provide the APK Name you want to download.*`;
 
       await conn.reply(m.chat, global.wait, m);
       let data = await download(text);
 
       if (data.size.replace(' MB', '') > 200) {
-        return await conn.sendMessage(m.chat, { text: '*⚠️The File Is Too Large.*' }, { quoted: m });
+        return await conn.sendMessage(m.chat, { text: '*[⛔] The file is too large.*' }, { quoted: m });
       }
 
       if (data.size.includes('GB')) {
-        return await conn.sendMessage(m.chat, { text: '*⚠️The File Is Too Large.*' }, { quoted: m });
+        return await conn.sendMessage(m.chat, { text: '*[⛔] The file is too large.*' }, { quoted: m });
       }
 
       await conn.sendMessage(
         m.chat,
         { document: { url: data.dllink }, mimetype: 'application/vnd.android.package-archive', fileName: data.name + '.apk', caption: null },
         { quoted: m }
-      );
+      )
     }
   } catch {
-    throw `*❌ لم أجد التطبيق الذي في الرابط حاول مجددا.*`;
+    throw `*[❗] An error occurred. Make sure to provide a valid link.*`;
   }
 };
 
+handler.help = ['modapk']
+handler.tags = ['downloader']
 handler.command = /^apk$/i;
 export default handler;
-  
