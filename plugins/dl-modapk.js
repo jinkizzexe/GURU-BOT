ccf1289ad5038d7e32ca7ed5a835abb0a9aa4413
OMAR ! <156843341+jinkizzexe@users.noreply.github.com>
2024-01-21 09:44:30 +0100

    Update dl-modapk.js

--- plugins/dl-modapk.js
+++ plugins/dl-modapk.js
@@ -3,17 +3,17 @@ import { download } from 'aptoide-scraper';
 let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
   try {
     if (command === 'apk') {
-      if (!text) throw `_*👾 سكوزا بوت يحتاج الى  إسم التطبيق الذي تريده*_ \n *INSTAGRAM* \n *https://www.instagram.com/ovmar_1* \n *Whatsapp Gp* \n *https://chat.whatsapp.com/ByIauCfwCFeE1ozctmwbS5 !*`;
+      if (!text) throw `_*👾 سكوزا بوت يحتاج الى  إسم التطبيق الذي تريده*_ \n \n \n *INSTAGRAM* \n *https://www.instagram.com/ovmar_1* \n *Whatsapp Gp* \n *https://chat.whatsapp.com/ByIauCfwCFeE1ozctmwbS5 !*`;
 
-      await conn.reply ( hey );
+      await conn.reply (m.chat, global.wait, m);
       let data = await download(text);
 
       if (data.size.replace(' MB', '') > 200) {
-        return await conn.sendMessage(m.chat, { text: '_*سكوزا بوت يقول ان التطبيق كبير لإرساله ( سمح ليا )*_ \n *INSTAGRAM* \n *https://www.instagram.com/ovmar_1* \n *Whatsapp Gp* \n *https://chat.whatsapp.com/ByIauCfwCFeE1ozctmwbS5 !*' }, { quoted: m });
+        return await conn.sendMessage(m.chat, { text: '_*سكوزا بوت يقول ان التطبيق كبير لإرساله ( سمح ليا )*_ \n \n \n *INSTAGRAM* \n *https://www.instagram.com/ovmar_1* \n *Whatsapp Gp* \n *https://chat.whatsapp.com/ByIauCfwCFeE1ozctmwbS5 !*' }, { quoted: m });
       }
 
       if (data.size.includes('GB')) {
-        return await conn.sendMessage(m.chat, { text: '_*سكوزا بوت يقول ان التطبيق كبير لإرساله ( سمح ليا )*_ \n *INSTAGRAM* \n *https://www.instagram.com/ovmar_1* \n *Whatsapp Gp* \n *https://chat.whatsapp.com/ByIauCfwCFeE1ozctmwbS5 !*' }, { quoted: m });
+        return await conn.sendMessage(m.chat, { text: '_*سكوزا بوت يقول ان التطبيق كبير لإرساله ( سمح ليا )*_ \n\n\n *INSTAGRAM* \n *https://www.instagram.com/ovmar_1* \n *Whatsapp Gp* \n *https://chat.whatsapp.com/ByIauCfwCFeE1ozctmwbS5 !*' }, { quoted: m });
       }
 
       await conn.sendMessage( hwy
