641bf66fc4c676cbec639fbdab3dd141971ec5ee
OMAR ! <156843341+jinkizzexe@users.noreply.github.com>
2024-01-21 07:44:17 +0100

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -536,7 +536,7 @@ import {
    
     global.fcontact = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: 'status@broadcast' }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     const infoText = `
-        ${botname} BAot
+        ${botname} BOT
     
     ${ucpn}
    
@@ -588,7 +588,7 @@ import {
   │ *17.* Religion Menu
   │ *18.* Plugin Menu
   ╰───────⳹
- ${readMore}` 
+ ${sourceUrl}` 
 ;
 
   
