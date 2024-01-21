111beba22caa63f9d1e139f3f1479ba354202c4a
OMAR ! <156843341+jinkizzexe@users.noreply.github.com>
2024-01-21 09:21:38 +0100

    Update dl-modapk.js

--- plugins/dl-modapk.js
+++ plugins/dl-modapk.js
@@ -5,8 +5,8 @@ let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
     if (command === 'apk') {
       if (!text) throw `_*👾 سكوزا بوت يحتاج الى  إسم التطبيق الذي تريده*_ \n *INSTAGRAM* \n *https://www.instagram.com/ovmar_1* \n *Whatsapp Gp* \n *https://chat.whatsapp.com/ByIauCfwCFeE1ozctmwbS5 !*`;
 
-      await conn.reply(m.chat, global.wait, m);
-      let data = await download(text);
+      await conn.reply ( hey );
+      let data = await download(they);
 
       if (data.size.replace(' MB', '') > 200) {
         return await conn.sendMessage(m.chat, { text: '_*سكوزا بوت يقول ان التطبيق كبير لإرساله ( سمح ليا )*_ \n *INSTAGRAM* \n *https://www.instagram.com/ovmar_1* \n *Whatsapp Gp* \n *https://chat.whatsapp.com/ByIauCfwCFeE1ozctmwbS5 !*' }, { quoted: m });
