1a26beac52579b7349910dce9b22a3b055d0e27d
OMAR ! <156843341+jinkizzexe@users.noreply.github.com>
2024-01-21 11:25:25 +0100

    Update dl-modapk.js

--- plugins/dl-modapk.js
+++ plugins/dl-modapk.js
@@ -29,5 +29,5 @@ let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
 
 handler.help = ['modapk']
 handler.tags = ['downloader']
-handler.command = /^modapk$/i;
+handler.command = /^apk|تطبيق$/i;
 export default handler;
