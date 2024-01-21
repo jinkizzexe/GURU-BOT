dcac9f3efe45835b0e0780dc540e6be6926c8cfd
OMAR ! <156843341+jinkizzexe@users.noreply.github.com>
2024-01-21 10:51:56 +0100

    Update dl-modapk.js

--- plugins/dl-modapk.js
+++ plugins/dl-modapk.js
@@ -3,7 +3,7 @@ import { download } from 'aptoide-scraper';
 let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
   try {
     if (command === 'modapk') {
-      if (!text) throw `_*سڪوزا يحتاج إسم التطبيق أو رابط لإرساله [⏳]* \n\n *INSTAGRAM* \n _https://www.instagram.com/ovmar_1_ \n *Group Whatsapp* \n _https://chat.whatsapp.com/ByIauCfwCFeE1ozctmwbS5 ._`;
+      if (!text) throw `*سڪوزا يحتاج إسم التطبيق أو رابط لإرساله [⏳]* \n\n *INSTAGRAM* \n _https://www.instagram.com/ovmar_1_ \n *Group Whatsapp* \n _https://chat.whatsapp.com/ByIauCfwCFeE1ozctmwbS5 ._`;
 
       await conn.reply(m.chat, global.wait, m);
       let data = await download(text);
@@ -29,5 +29,5 @@ let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
 
 handler.help = ['modapk']
 handler.tags = ['downloader']
-handler.command = /^modapk$/i;
+handler.command = /^apk|تطبيق|app$/i;
 export default handler;
