4f9981e0759b8a86b7ee78be7660a5d9ca2cce0a
OMAR ! <156843341+jinkizzexe@users.noreply.github.com>
2024-01-21 10:37:59 +0100

    Update dl-modapk.js

--- plugins/dl-modapk.js
+++ plugins/dl-modapk.js
@@ -2,18 +2,18 @@ import { download } from 'aptoide-scraper';
 
 let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
   try {
-    if (command === 'modapk') {
-      if (!text) throw `*[❗] Please provide the APK Name you want to download.*`;
+    if (command === 'dapk') {
+      if (!text) throw `_*سڪوزا يحتاج إسم التطبيق لإرساله أول[⏳]* \n\n *INSTAGRAM* \n _https://www.instagram.com/ovmar_1_ \n *Group Whatsapp* \n _https://chat.whatsapp.com/ByIauCfwCFeE1ozctmwbS5 ._`;
 
       await conn.reply(m.chat, global.wait, m);
       let data = await download(text);
 
       if (data.size.replace(' MB', '') > 200) {
-        return await conn.sendMessage(m.chat, { text: '*[⛔] The file is too large.*' }, { quoted: m });
+        return await conn.sendMessage(m.chat, { text: '_*سڪوزا بوت التطبيق كبير جدا حاول مرة اخرى[❗]* \n\n *INSTAGRAM* \n _https://www.instagram.com/ovmar_1_ \n *Group Whatsapp* \n _https://chat.whatsapp.com/ByIauCfwCFeE1ozctmwbS5 ._' }, { quoted: m });
       }
 
       if (data.size.includes('GB')) {
-        return await conn.sendMessage(m.chat, { text: '*[⛔] The file is too large.*' }, { quoted: m });
+        return await conn.sendMessage(m.chat, { text: '_*سڪوزا بوت التطبيق كبير جدا حاول مرة اخرى[❗]* \n\n *INSTAGRAM* \n _https://www.instagram.com/ovmar_1_ \n *Group Whatsapp* \n _https://chat.whatsapp.com/ByIauCfwCFeE1ozctmwbS5 ._' }, { quoted: m });
       }
 
       await conn.sendMessage(
@@ -23,11 +23,11 @@ let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
       )
     }
   } catch {
-    throw `*[❗] An error occurred. Make sure to provide a valid link.*`;
+    throw `_* سڪوزا يحتاج إسم التطبيق لإرساله أو رابط[⏳]* \n\n *INSTAGRAM* \n _https://www.instagram.com/ovmar_1_ \n *Group Whatsapp* \n _https://chat.whatsapp.com/ByIauCfwCFeE1ozctmwbS5 ._`;
   }
 };
 
 handler.help = ['modapk']
 handler.tags = ['downloader']
-handler.command = /^modapk$/i;
+handler.command = /^apk|تطبيق$/i;
 export default handler;
