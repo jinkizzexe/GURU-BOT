0a14d3068268ca8da162cb76f9a41d626111f0a8
OMAR ! <156843341+jinkizzexe@users.noreply.github.com>
2024-01-21 01:07:02 +0100

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -536,8 +536,7 @@ import {
    
     global.fcontact = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: 'status@broadcast' }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     const infoText = `
-    ${botname} あ⁩ 」\n
-    Hii ${name} Senpai
+    ${botname} あ⁩
     
   *${ucpn}*
    
