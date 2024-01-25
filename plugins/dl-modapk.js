e3c1fc0c331e2784aea1be70953ac706f1a1f6e8
OMAR ! <156843341+jinkizzexe@users.noreply.github.com>
2024-01-25 08:57:11 +0100

    Add files via upload

---
+++ plugins/dl-modapk.js
@@ -0,0 +1,35 @@
+import { download } from 'aptoide-scraper';
+
+let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
+  try {
+    if (command === 'modapk', 'apk', 'app') {
+      if (!text) throw `𝘓3𝘻𝘸𝘢𝘢 𝘣𝘰𝘵 \n \n _*لم نتڪمن من التعرف على اسم او رابط التطبيق 🔎*_ \n 𝘐𝘯𝘴𝘵𝘢𝘨𝘳𝘢𝘮 \n  _https://www.instagram.com/ovmar_1_  𝘞𝘩𝘢𝘵𝘴𝘢𝘱𝘱 𝘎𝘗 \n _https://chat.whatsapp.com/ByIauCfwCFeE1ozctmwbS5 :_`;
+      m.react(rwait)
+      await conn.reply(m.chat, global.wait, m);
+      let data = await download(text);
+
+      if (data.size.replace(' MB', '') > 700) {
+        return await conn.sendMessage(m.chat, { text: '𝘓3𝘻𝘸𝘢𝘢 𝘣𝘰𝘵 \n \n _*التطبيق ڪبير جدا 🚫*_ \n 𝘐𝘯𝘴𝘵𝘢𝘨𝘳𝘢𝘮 \n  _https://www.instagram.com/ovmar_1_  𝘞𝘩𝘢𝘵𝘴𝘢𝘱𝘱 𝘎𝘗 \n _https://chat.whatsapp.com/ByIauCfwCFeE1ozctmwbS5 :_' }, { quoted: m });
+      }
+
+      if (data.size.includes('GB')) {
+        return await conn.sendMessage(m.chat, { text: '𝘓3𝘻𝘸𝘢𝘢 𝘣𝘰𝘵 \n \n _*التطبيق ڪبير جدا 🚫*_ \n 𝘐𝘯𝘴𝘵𝘢𝘨𝘳𝘢𝘮 \n  _https://www.instagram.com/ovmar_1_  𝘞𝘩𝘢𝘵𝘴𝘢𝘱𝘱 𝘎𝘗 \n _https://chat.whatsapp.com/ByIauCfwCFeE1ozctmwbS5 :_' }, { quoted: m });
+      }
+
+      await conn.sendMessage(
+        m.chat,
+        { document: { url: data.dllink }, mimetype: 'application/vnd.android.package-archive', fileName: data.name + '.apk', caption: null },
+        { quoted: m }
+      )
+    }
+  m.react(done)
+  } catch {
+    throw `
+    𝘓3𝘻𝘸𝘢𝘢 𝘣𝘰𝘵 \n \n _*لم نتڪمن من التعرف على اسم او رابط التطبيق 🔎*_ \n 𝘐𝘯𝘴𝘵𝘢𝘨𝘳𝘢𝘮 \n  _https://www.instagram.com/ovmar_1_  𝘞𝘩𝘢𝘵𝘴𝘢𝘱𝘱 𝘎𝘗 \n _https://chat.whatsapp.com/ByIauCfwCFeE1ozctmwbS5 :_`;
+  }
+};
+
+handler.help = ['modapk', 'apk', 'app', 'تطبيق']
+handler.tags = ['downloader']
+handler.command = ['modapk', 'apk', 'app', 'تطبيق'];
+export default handler;
