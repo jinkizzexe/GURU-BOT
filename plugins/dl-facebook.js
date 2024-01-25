0b89ace13538fabd9e13ffc947ae1f5088f2722e
OMAR ! <156843341+jinkizzexe@users.noreply.github.com>
2024-01-25 18:40:42 +0100

    Update dl-facebook.js

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -14,10 +14,7 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
 
   try {
     const result = await fg.fbdl(args[0]);
-    const tex = `
-⊱ ─── {*L3ZWA FBDL*} ─── ⊰
-↳ *وقت الفيديو:* ${result.title}
-⊱ ────── {⋆♬⋆} ────── ⊰`;
+    const tex = `*ثم إرسال الفيديو بنجاح* \n instagram \n https://www.instagram.com/ovmar_1`;
 
     const response = await fetch(result.videoUrl);
     const arrayBuffer = await response.arrayBuffer();
