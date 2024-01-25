18b61a2f7258f261a14843e612edd910c1f443a9
OMAR ! <156843341+jinkizzexe@users.noreply.github.com>
2024-01-25 11:00:40 +0100

    Update dl-spotify.js

--- plugins/dl-spotify.js
+++ plugins/dl-spotify.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, text }) => {
     }
   m.react('🎶')
   await displayLoadingScreen(conn, m.chat);
-  let pp = 'https://wallpapercave.com/wp/wp7932387.jpg'
+  let pp = 'https://i.imgur.com/9BcXaZs.jpg'
     const query = encodeURIComponent(text);
     let res = `https://guruapi.tech/api/spotifydl?url=${query}`
    // let spotify = await (await fetch(res)).buffer()
